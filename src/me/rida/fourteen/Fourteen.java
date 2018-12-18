ca5305b111d67a02a8e1030760e0757d2c62982d
ridalarry <rida.larry@live.com>
2018-12-18 07:12:14 +0300

    optimizations
    Using async entirely
    removed unneeded code

--- src/me/rida/fourteen/Fourteen.java
+++ src/me/rida/fourteen/Fourteen.java
@@ -1,54 +1,38 @@
 package me.rida.fourteen;
 
-import java.io.File;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Fourteen extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		File file = new File(getDataFolder(), "config.yml");
-		if (!file.exists()) {
+		if (this.getConfig().getString("count") == null) {
 			this.getConfig().addDefault("count", 10);
-			this.getConfig().addDefault("noPerm", "&cNo permission!");
-			this.getConfig().addDefault("done", "&aDone!");
-			this.getConfig().addDefault("spam", "14");
-			this.getConfig().addDefault("permission", "fourteen.use");
-			this.getConfig().addDefault("unknown", "&cUnknown argument!");
-			this.getConfig().addDefault("reload", "&aReloaded!");
 		}
-		else {
-			if (this.getConfig().getString("count") == null) {
-				this.getConfig().addDefault("count", 10);
-			}
-			if (this.getConfig().getString("noPerm") == null) {
-				this.getConfig().addDefault("noPerm", "&cNo permission!");
-			}
-			if (this.getConfig().getString("done") == null) {
-				this.getConfig().addDefault("done", "&aDone!");
-			}
-			if (this.getConfig().getString("spam") == null) {
-				this.getConfig().addDefault("spam", "14");
-			}
-			if (this.getConfig().getString("permission") == null) {
-				this.getConfig().addDefault("permission", "fourteen.use");
-			}
-			if (this.getConfig().getString("unknown") == null) {
-				this.getConfig().addDefault("unknown", "&cUnknown argument!");
-			}
-			if (this.getConfig().getString("reload") == null) {
-				this.getConfig().addDefault("reload", "&aReloaded!");
-			}
+		if (this.getConfig().getString("noPerm") == null) {
+			this.getConfig().addDefault("noPerm", "&cNo permission!");
+		}
+		if (this.getConfig().getString("done") == null) {
+			this.getConfig().addDefault("done", "&aDone!");
+		}
+		if (this.getConfig().getString("spam") == null) {
+			this.getConfig().addDefault("spam", "14");
+		}
+		if (this.getConfig().getString("permission") == null) {
+			this.getConfig().addDefault("permission", "fourteen.use");
+		}
+		if (this.getConfig().getString("unknown") == null) {
+			this.getConfig().addDefault("unknown", "&cUnknown argument!");
+		}
+		if (this.getConfig().getString("reload") == null) {
+			this.getConfig().addDefault("reload", "&aReloaded!");
 		}
 		this.getConfig().options().copyDefaults(true);
 		saveConfig();
-		System.out.println("[Fourteen] has been enabled!");
 		this.getCommand("14").setExecutor(new FourteenCommand(this));
 	}
 
 	@Override
 	public void onDisable() {
-		System.out.println("[Fourteen] has been disabled!");
 	}
 }
--- src/me/rida/fourteen/FourteenCommand.java
+++ src/me/rida/fourteen/FourteenCommand.java
@@ -10,54 +10,40 @@ import org.bukkit.scheduler.BukkitRunnable;
 import me.rida.fourteen.Fourteen;
 
 public class FourteenCommand implements CommandExecutor {
-	public Fourteen Fourteen;
-	public FourteenCommand(Fourteen Fourteen) {
+	private Fourteen Fourteen;
+	protected FourteenCommand(Fourteen Fourteen) {
 		this.Fourteen = Fourteen;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender s, Command c, String a, String[] g) {
-		if (!s.hasPermission(Fourteen.getConfig().getString("permission"))) {
-			s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("noPerm")));
-			return true;
-		}
-		if(g.length == 1) {
-			if (g[0].equalsIgnoreCase("reload")) {
-				Fourteen.getConfig().getString("noPerm");
-				Fourteen.getConfig().getString("done");
-				Fourteen.getConfig().getString("spam");
-				Fourteen.getConfig().getString("permission");
-				Fourteen.getConfig().getString("unknown");
-				Fourteen.getConfig().getLong("count");
-				Fourteen.getConfig().getLong("reload");
-				s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("reload")));
-				Fourteen.reloadConfig();
-				Fourteen.saveConfig();
-				return true;
-			}
-			else {
-				s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("unknown")));
-				return true;
-			}
-		}
-		if(g.length == 0) {
-			new BukkitRunnable() {
-				@Override
-				public void run() {
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				if (!s.hasPermission(Fourteen.getConfig().getString("permission"))) {
+					s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("noPerm")));
+				}
+				else if (g.length == 1) {
+					if (g[0].equalsIgnoreCase("reload")) {
+						Fourteen.reloadConfig();
+						Fourteen.saveConfig();
+						s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("reload")));
+					}
+					else {
+						s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("unknown")));
+					}
+				}
+				else if (g.length == 0) {
 					long count;
 					for (count = Fourteen.getConfig().getLong("count") - 1; count >=0; --count) {
 						String countAsString = Long.toString(count+1);
-						Bukkit.getOnlinePlayers().forEach(p -> p.chat(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("spam").replaceAll("%counter%", countAsString).replaceAll("%sender%", s.getName()).replaceAll("%player%", p.getName()))));
+						String spam = Fourteen.getConfig().getString("spam");
+						Bukkit.getOnlinePlayers().forEach(p -> p.chat(ChatColor.translateAlternateColorCodes('&', spam.replaceAll("%counter%", countAsString).replaceAll("%sender%", s.getName()).replaceAll("%player%", p.getName()))));
 					}
 					s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("done")));
 				}
-
-			}.runTaskAsynchronously(Fourteen);
-			return true;
-		}
-		else {
-			s.sendMessage(ChatColor.translateAlternateColorCodes('&', Fourteen.getConfig().getString("unknown")));
-			return true;
-		}
+			}
+		}.runTaskAsynchronously(Fourteen);
+		return true;
 	}
 }
